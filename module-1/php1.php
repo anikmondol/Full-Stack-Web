3c22418bcbe75fc8a119919a141bd1782d28d60c
anikmondol <anikmondol558363@gmail.com>
2025-07-04 23:22:47 +0600

    Full Stack Web Development with PHP, Laravel & Vue Js Journey module-1 100%

---
+++ module-1/php1.php
@@ -0,0 +1,72 @@
+<?php
+
+// echo "Hello World";
+
+
+// echo PHP_EOL;
+
+// $name = "anik";
+
+// echo $name;
+
+
+// const name = "anik";
+
+
+// define("name", "anik");
+
+// echo name;
+
+
+
+// this is single line comment
+
+
+/*
+
+this is multiline comment
+
+*/
+
+
+// output pringint
+
+
+// echo "this print from echo <br>";
+
+// print "this print from print <br>";
+
+// printf("this print from printf <br>");
+
+// sprintf("this print from sprintf <br>");
+
+
+$price = 99.9;
+
+
+// printf("price %.2f", $price);
+
+// printf("price %.d", $price);
+
+
+// $princeFormated = sprintf("Price : %.2f", $price);
+
+// echo $princeFormated;
+
+
+// Data Types
+
+/*
+
+1. string
+2.  
+3. bool
+4. array
+5. object
+
+
+
+*/ 
+
+
+?>
