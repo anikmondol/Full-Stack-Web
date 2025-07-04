7c511234eb41ee1061af96208b7ada267feed34a
anikmondol <anikmondol558363@gmail.com>
2025-07-05 01:13:39 +0600

    Full Stack Web Development with PHP, Laravel & Vue Js Journey module-2 100%

---
+++ module-2/php1.php
@@ -0,0 +1,135 @@
+<?php
+
+// Arithmetice Operators
+
+
+/*
+
++, -, *, /, %
+
+*/ 
+
+
+// $a = 10;
+// $b = 5;
+
+
+// $sum = $a + $b;
+// $sub = $a - $b;
+// $mul = $a * $b;
+// $div = $a / $b;
+// $mod = $a % $b;
+
+
+// echo "sum : $sum <br>";
+// echo "sub : $sub <br>";
+// echo "mul : $mul <br>";
+// echo "div : $div <br>";
+// echo "sum : $mod <br>";
+
+
+
+// Assigment Operators
+
+/*
+
+=, +=, -=, *=, /=, %= 
+
+*/ 
+
+
+// $a = 10;
+
+
+// echo $a += 5;  // shortForm --> $a = $a + 5;
+// echo "<br>";
+
+// echo $a -= 5;  // shortForm --> $a = $a - 5;
+// echo "<br>";
+
+// echo $a *= 5;  // shortForm --> $a = $a * 5;
+// echo "<br>";
+
+
+// echo $a /= 5;  // shortForm --> $a = $a / 5;
+// echo "<br>";
+
+// echo $a %= 5;  // shortForm --> $a = $a % 5;
+// echo "<br>";
+
+
+// Comparison Operators
+
+/*
+
+==, ===, !=, !==
+
+*/ 
+
+
+// $x = 10;
+// $y = 10;
+
+// var_dump($x == $y); // value compare
+// var_dump($x === $y); // value compare with type
+
+
+// Logical Operators
+
+/*
+
+>, <, <=, >=
+
+*/ 
+
+// $x = 10;
+// $y = 10;
+
+// var_dump($x > $y); 
+// echo "<br>";
+// var_dump($x < $y); 
+
+// var_dump($x >= $y); 
+// echo "<br>";
+// var_dump($x <= $y); 
+
+
+
+// Increment/Decrement Operators
+
+/*
+
+++, --
+
+*/ 
+
+
+
+// Conditional Statements
+// if else, else if
+
+
+// $age = 18;
+
+// if ($age >= 18) {
+//     echo "you are adult";
+// } else {
+//     echo "you are not adult";
+// }
+
+
+// ternary Operator
+
+/*
+
+?,:
+
+*/ 
+
+$a = 17;
+
+echo $a >= 18 ? "You are Adult" : "You are Not Adult"; 
+
+
+
+?>
