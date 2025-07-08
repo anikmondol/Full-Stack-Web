51a349b6e8909870b8adf26ed3a04798053d5c48
anikmondol <anikmondol558363@gmail.com>
2025-07-08 09:21:43 +0600

    Full-Stack-Web modle-2 ar live-class-1 fulll complate

---
+++ Module/live-class-3/php1.php
@@ -0,0 +1,87 @@
+<?php
+
+
+// for loop
+// for (initialization; condition; increment/decrement) { 
+//     loop statement;
+// }
+
+
+
+// for ($i=1; $i <= 10; $i++) { 
+//     echo $i . "<br>";
+// }
+
+
+
+//  while loop
+//  initialization = value;
+
+//  while (condtion) {
+//     loop statement
+//     increment/decrement
+//  }
+
+
+// $i = 1;
+
+// while ($i <= 10) {
+//     echo $i . "<br>";
+//     $i++;
+// }
+
+
+// foreach
+
+// $arr = [1, 5, 7];
+
+// foreach ($arr as $key => $value) {
+//     echo $value . "<br>";
+// }
+
+
+// do while
+
+/*
+
+$i = 1;
+
+do{
+
+echo $i;
+$i++;
+
+}while($i <= 10)
+
+*/ 
+
+// $i = 1;
+
+// do {
+//    echo $i . "<br>";
+//    $i++;
+// } while ($i <= 10);
+
+
+// continue
+
+
+// for ($i=1; $i <= 10; $i++) { 
+//     if ($i == 5) {
+//         continue;
+//     }
+
+//     echo $i . "<br>";
+// }
+
+
+// break
+
+
+// for ($i=1; $i <= 10; $i++) { 
+//     if ($i == 5) {
+//         break;
+//     }
+
+//     echo $i . "<br>";
+// }
---
+++ Module/live-class-3/php2.php
@@ -0,0 +1,28 @@
+<?php
+
+
+// function hello() {
+    
+//     echo "hello from function";
+
+// }
+
+// hello();
+
+
+// function sum($a, $b) { //parameters
+//     echo $a + $b;
+// }
+
+// sum(10, 5); // arguments
+
+// build-in function 
+$colors = ['red', 'green', 'blue', 'yellow'];
+
+for ($i=0; $i < count($colors); $i++) { 
+   echo $colors[$i] . "<br>";
+}
+
+
+
+?>
