f319c1442240e8b2b48e6843d7f5defa6003f3c1
anikmondol <anikmondol558363@gmail.com>
2025-08-24 01:26:53 +0600

    Data Structures and Algorithms class Introduction to DSA and PHP Basics 100% done

---
+++ Data-Structures-and-Algorithms/DSA-And-PHP-Basics/DSA-And-PHP-Basics(1)/analytics.php
@@ -0,0 +1,28 @@
+<?php
+
+
+## 1 .....100 sum
+
+// $sum = 0;
+
+// for ($i=1; $i <= 100; $i++) {  // execution time O(n)
+//     $sum += $i;
+// }
+
+// echo $sum;
+
+
+
+## equation
+
+$n = 100;
+
+
+$sum = $n * ($n + 1) / 2; // // execution time O(1)
+
+
+echo $sum;
+
+
+
+?>
