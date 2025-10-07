98c72961e770213f705d220b673e4f12e21de108
anikmondol <anikmondol558363@gmail.com>
2025-10-07 09:31:00 +0600

    m12c2 50 mins done done

--- Module/M12C1/index.js
+++ Module/M12C1/index.js
@@ -12,47 +12,52 @@
 // console.log(Math.random()); // Random number between 0 and 1
 // console.log(Math.floor(Math.random() * 10) + 1); // Random number between 1 and 10
 // console.log(Math.log(1));
-console.log(Math.log2(8)); // 3
-console.log(Math.log10(100)); // 2
-console.log(Math.exp(2)); // e^2
-console.log(Math.cbrt(27)); // 3
-console.log(Math.sinh(0));
-console.log(Math.cosh(0));
-console.log(Math.tanh(1));
-console.log(Math.asin(1));
-console.log(Math.acos(1));
-console.log(Math.atan(1));
-console.log(Math.atan2(1, 1)); // π/4
-console.log(Math.hypot(3, 4)); // 5
-console.log(Math.clz32(1));
-console.log(Math.imul(2, 4)); // 8
-console.log(Math.fround(1.5)); // 1.5
-console.log(Math.expm1(1)); // e^1 - 1
-console.log(Math.log1p(1));
-console.log(Math.signbit(-3)); // true
-console.log(Math.sgn(3));
-console.log(Math.sgn(-3));
-console.log(Math.sgn(0));
-console.log(Math.sgn(-0));
-console.log(Math.sgn(NaN));
-console.log(Math.sgn(Infinity));
-console.log(Math.sgn(-Infinity));
-console.log(Math.sgn(42));
-console.log(Math.sgn(-42));
-console.log(Math.sgn(0.0001));
-console.log(Math.sgn(-0.0001));
-console.log(Math.sgn(1e10));
-console.log(Math.sgn(-1e10));
-console.log(Math.sgn(3.14));
-console.log(Math.sgn(-3.14));
-console.log(Math.sgn(1.7976931348623157e+308));
-console.log(Math.sgn(-1.7976931348623157e+308));
-console.log(Math.sgn(5e-324));
-console.log(Math.sgn(-5e-324));
-console.log(Math.sgn(123456789));
-console.log(Math.sgn(-123456789));
-console.log(Math.sgn(0.1));
-console.log(Math.sgn(-0.1));
-console.log(Math.sgn(999999999));
-console.log(Math.sgn(-999999999));
-console.log(Math.sgn(2.71828));
+// console.log(Math.log2(8)); // 3
+// console.log(Math.log10(100)); // 2
+// console.log(Math.exp(2)); // e^2
+// console.log(Math.cbrt(27)); // 3
+// console.log(Math.sinh(0));
+// console.log(Math.cosh(0));
+// console.log(Math.tanh(1));
+// console.log(Math.asin(1));
+// console.log(Math.acos(1));
+// console.log(Math.atan(1));
+// console.log(Math.atan2(1, 1)); // π/4
+// console.log(Math.hypot(3, 4)); // 5
+// console.log(Math.clz32(1));
+// console.log(Math.imul(2, 4)); // 8
+// console.log(Math.fround(1.5)); // 1.5
+// console.log(Math.expm1(1)); // e^1 - 1
+// console.log(Math.log1p(1));
+// console.log(Math.signbit(-3)); // true
+// console.log(Math.sgn(3));
+// console.log(Math.sgn(-3));
+// console.log(Math.sgn(0));
+// console.log(Math.sgn(-0));
+// console.log(Math.sgn(NaN));
+// console.log(Math.sgn(Infinity));
+// console.log(Math.sgn(-Infinity));
+// console.log(Math.sgn(42));
+// console.log(Math.sgn(-42));
+// console.log(Math.sgn(0.0001));
+// console.log(Math.sgn(-0.0001));
+// console.log(Math.sgn(1e10));
+// console.log(Math.sgn(-1e10));
+// console.log(Math.sgn(3.14));
+// console.log(Math.sgn(-3.14));
+// console.log(Math.sgn(1.7976931348623157e+308));
+// console.log(Math.sgn(-1.7976931348623157e+308));
+// console.log(Math.sgn(5e-324));
+// console.log(Math.sgn(-5e-324));
+// console.log(Math.sgn(123456789));
+// console.log(Math.sgn(-123456789));
+// console.log(Math.sgn(0.1));
+// console.log(Math.sgn(-0.1));
+// console.log(Math.sgn(999999999));
+// console.log(Math.sgn(-999999999));
+// console.log(Math.sgn(2.71828));
+
+
+// window.open("https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math", "_blank");
+console.log(window.innerHeight);
+console.log(window.innerWidth);
